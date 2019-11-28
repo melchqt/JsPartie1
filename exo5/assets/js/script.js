71398795b8563fdcbee1094c62afe8260cf0c029
melchqt <57181754+melchqt@users.noreply.github.com>
2019-11-28 12:02:03 +0100

    exo

---
+++ exo5/assets/js/script.js
@@ -0,0 +1,6 @@
+function division(){
+  var numberOne = document.getElementById('numberOne').value;
+  var numberTwo = document.getElementById('numberTwo').value;
+  var result = numberOne % numberTwo
+  alert(result);
+}
